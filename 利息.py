e24c76642e8c0579542c01d1c7104e3ea5ee6013
ruilin <110581221+69936876@users.noreply.github.com>
2023-06-20 16:47:06 +0800

    Update 利息.py

--- 利息.py
+++ 利息.py
@@ -1,5 +1,12 @@
-x=int(input("请输入金额："))
-y=x*3.65/100
-print("利息是：",y)            
-          
+print("1、计算利息")
+print("2、关于本程序")
+choice = input("输入你的选择(1/2):")
+if choice =="1":
+ x=int(input("请输入金额："))
+elif choice == "2":
+  int(input(1.0))
+elif int(input(y=x*2.5/200)):
+     print("利息是：",y)
+print("已结束会话，请重新打开本程序")
+
             
