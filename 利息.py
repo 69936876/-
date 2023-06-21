6834efbc21b6dd1c2f2306acf532907e69a56f0b
ruilin <110581221+69936876@users.noreply.github.com>
2023-06-21 11:20:07 +0800

    Update 利息.py

--- 利息.py
+++ 利息.py
@@ -1,12 +1,20 @@
 print("1、计算利息")
-print("2、关于本程序")
-choice = input("输入你的选择(1/2):")
+print("2、程序版本")
+print("3、关于本程序")
+choice = input("输入你的选择(1/2/3):")
 if choice =="1":
- x=int(input("请输入金额："))
+    x=int(input("请输入金额："))
+    y = x * 2.5 / 200
+    print("利息是：", y)
+    print("（程序已结束运行）")
 elif choice == "2":
-  int(input(1.0))
-elif int(input(y=x*2.5/200)):
-     print("利息是：",y)
-print("已结束会话，请重新打开本程序")
+     int(input("版本：1.5增强版"))
+     print("（程序已结束运行）")
+elif choice == "3":
+     print("使用本程序可以计算1年的利息，数值来源于互联网")
+     print("(本程序仅供参考，请以各大银行官网和柜台公布的利息为准)如有问题，请咨询银行客服")
+     print("（程序已结束运行）")
+
+
 
             
